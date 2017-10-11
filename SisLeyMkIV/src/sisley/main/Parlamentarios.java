1069224718670425cc5acc3bfa78c0ea1f78fe3c
ChutLuis <l.chuta2014ig@gmail.com>
2017-10-11 16:04:34 -0600

    Interfaz y clases

---
+++ SisLeyMkIV/src/sisley/main/Parlamentarios.java
@@ -0,0 +1,19 @@
+package sisley.main;
+
+public class Parlamentarios {
+	private String usuario;
+	private int idPar;
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public int getIdPar() {
+		return idPar;
+	}
+	public void setIdPar(int idPar) {
+		this.idPar = idPar;
+	}
+	
+}
