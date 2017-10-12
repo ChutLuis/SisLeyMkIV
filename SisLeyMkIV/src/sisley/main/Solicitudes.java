d9ec7fc4db6f73a8465aa2b8aa942df9441d1fa7
ChutLuis <l.chuta2014ig@gmail.com>
2017-10-11 18:47:58 -0600

    Interfaz y clases

--- SisLeyMkIV/src/sisley/main/Solicitudes.java
+++ SisLeyMkIV/src/sisley/main/Solicitudes.java
@@ -4,21 +4,55 @@ public class Solicitudes {
 	private String ley;
 	private int idLey;
 	private int idReg;
-	private String reglamento;]
-	ArrayList<Parlamentarios> n1= new ArrayList<Parlamentarios>();
-	
-	public void prestaReglamento(int idReg, String Reg, int copias, Parlamentarios e) {
-		int indexCola=0;
+	private String reglamento;
+	private Parlamentarios p1;
+	private int indexCola=0;
+	ArrayList<Parlamentarios> colaReglamentos= new ArrayList<Parlamentarios>();
+	ArrayList<Parlamentarios> colaLeyes= new ArrayList<Parlamentarios>();
+	public void prestaReglamento(int idReg, int copias, Parlamentarios e, Reglamentos a) {
+		
 		if(copias <=2)
-		{			
-		    	Parlamentarios.
+		{			e.copiaLey(idReg, copias, a);
+		    	
 		}
 		else
 		{
-			n1.add(indexCola, e);
+			colaReglamentos.add(indexCola, e);
 	    	indexCola++;
 		}
 	}
+	public void prestaLey(int idLey, int copias, Parlamentarios e, Leyes a) {
+		
+		if(copias <=2)
+		{			e.copiaLey(idReg, copias, a);
+		    	
+		}
+		else
+		{
+			colaLeyes.add(indexCola, e);
+	    	indexCola++;
+		}
+	}
+	private void prestaStack(int idLey, int copias, Parlamentarios e , Leyes a, Reglamentos b)
+	{
+		
+		if(copias <=2)
+		{			e.copiaStack(idLey, copias, a, b);
+		    	
+		}
+		else
+		{
+			colaLeyes.add(indexCola, e);
+			indexCola++;
+		}
+	    	
+	}
+	public void infoLey(int idLey, Leyes e, Reglamentos b)
+	{
+		
+	}
+	
+	
 
  
 }
